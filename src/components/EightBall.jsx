f1187f02f019a632c9a35201ef8be3b901b2f0e1
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-15 10:19:03 -0500

    initial commit

---
+++ src/components/EightBall.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const EightBall = (props) => {
+  return (
+    <>
+      <h1 className="">Magic Eightball</h1>
+      <form action="">
+        <label htmlFor="" className="">
+          What is your question???
+          <input type="text" className="" />
+        </label>
+        <button className="" type='submit'>Ask the Maigc 8 Ball</button>
+      </form>
+    </>
+  );
+};
+
+export default EightBall;
